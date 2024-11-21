41d24ab941cbc5b2eb7fec00c707f7bb7ac567fa
nathan shearer <help@rapidstartup.io>
2024-11-22 01:11:21 +0800

    fix layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 export default function RootLayout({
@@ -10,12 +10,6 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
           {children}
         </body>
       </html>
