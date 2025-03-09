0e8e9f5ac075ea13ad2b24cc83744929b076a3e0
Nathan Shearer <help@rapidstartup.io>
2025-03-09 16:29:10 +0800

    add google tag mgr scripts

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -52,6 +52,18 @@ export default function RootLayout({
           <PlausibleProvider domain="logo-creator.io" />
           <link rel="icon" href="/favicon.ico" sizes="any" />
           <meta name="color-scheme" content="dark" />
+          {/* Google tag (gtag.js) */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11180640635"></script>
+          <script 
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'AW-11180640635');
+              `
+            }}
+          />
         </head>
         <body
           className={`${jura.variable} dark min-h-full bg-[#343434] font-jura antialiased`}
